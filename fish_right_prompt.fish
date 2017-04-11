a643dda4aa380d207867994ca94152c783f0d6f9
bilal-bhatti <bilal-bhatti@users.noreply.github.com>
2017-04-10 23:20:41 -0500

    Update fish_right_prompt.fish

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -8,18 +8,3 @@ function fish_right_prompt
 	  echo -n -s "$pure_color_cyan$icon_node$pnv$pure_color_normal"
   end
 end
-
-function __project_node_version --description 'traverse up to find .nvmrc/version file, upto home directory'
-  set -l path (pwd)
-
-  while test -n (string trim $path); and test $path != $HOME
-    if test -f $path/.nvmrc
-      set nv (string trim -l -c=v (cat $path/.nvmrc 2>/dev/null))
-      echo -n -s $nv
-      return 0
-    end
-		set path (dirname $path)
-  end
-
-  return 0
-end
