be2820612dc0f1b8e7885921455d6aeb5c0b93cf
Bilal Bhatti <bilal.bhatti@ca.com>
2017-04-22 18:43:08 -0500

    fixed .nvmrc find up function

--- __project_node_version.fish
+++ __project_node_version.fish
@@ -1,13 +1,13 @@
 function __project_node_version --description 'traverse up to find .nvmrc/version file, upto home directory'
   set -l path (pwd)
 
-  while test -n (string trim $path); and test $path != $HOME
-    if test -f $path/.nvmrc
-      set nv (string trim -l -c=v (cat $path/.nvmrc 2>/dev/null))
-      echo -n -s $nv
-      return 0
-    end
-		set path (dirname $path)
+  while begin; test "$path" != ""; and not test -e $path/.nvmrc; end
+	  set path (echo $path | sed -e 's/\/[^\/]*$//')
+  end
+
+  if test -e $path/.nvmrc
+	  set nv (string trim -l -c=v (cat $path/.nvmrc 2>/dev/null))
+	  echo -n -s $nv
   end
 
   return 0
